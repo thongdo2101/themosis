7e3a895ed9b47a208f98e312e5f57392e67dfebd
thong <thong@thongs-MacBook-Pro.local>
2019-10-02 05:30:09 +0700

    add fine-tune-wp-config

--- config/wordpress.php
+++ config/wordpress.php
@@ -19,14 +19,14 @@
 | @link https://api.wordpress.org/secret-key/1.1/salt/
 |
 */
-define('AUTH_KEY', 'put your unique phrase here');
-define('SECURE_AUTH_KEY', 'put your unique phrase here');
-define('LOGGED_IN_KEY', 'put your unique phrase here');
-define('NONCE_KEY', 'put your unique phrase here');
-define('AUTH_SALT', 'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT', 'put your unique phrase here');
-define('NONCE_SALT', 'put your unique phrase here');
+define('AUTH_KEY',         '@;y/l<?&3E&JChbA:%wD.J)3iOMr+_iB8FtQ|%^lh+Qo(H}.|Ahh@@Lp!!TYhX|1');
+define('SECURE_AUTH_KEY',  ' .45>a>&!`*XXz-kv/z03(R5~3|xil_X|F@0+t+JV~Hc8sd]`;rpb]WqJ)-bm#bu');
+define('LOGGED_IN_KEY',    'aPI.^P[@tbHO+E FJh4L2{~+]cB=Nti2Fn};xG3SDM[(VdS-pau}|oZ=zT%LEd8(');
+define('NONCE_KEY',        'zmql([<+)h:oADZ2=p=l=ZXPWam7&z+NNGf,;M+m,RnE*-/-vrd JaFWOau45Aa?');
+define('AUTH_SALT',        'viuE}1$U`K>~@N_vNH^N6/+LMu_Ntw)otN8&<i3OR+H{ac;O%d*-8X6;YEUnRgj!');
+define('SECURE_AUTH_SALT', '|)(# 6>+Dly%7_J(%[Ax{#]2Ovj=l$ntO#+zYznCk;zAR^zZal- w-D&Gv#z.9~Z');
+define('LOGGED_IN_SALT',   'iJM~>`6Ow`R|BcSC}L&qOWViYeT%69|iA#|&ca<.E-H8k]CLKDN-*EKw.tvi7!j ');
+define('NONCE_SALT',       '&m55PH}hP9U8/U?}&KT5=+]|P!,k=`:]Bsft(q=tIXQ]39%:YP^2UU`yS~U(2-9K');
 
 /*
 |--------------------------------------------------------------------------
